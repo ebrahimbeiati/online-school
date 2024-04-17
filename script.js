4e9a55da1a33c344c904236ef328325849245f92
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2024-04-17 01:08:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll(".feature").forEach((feature) => {
+  feature.addEventListener("click", (event) => {
+    // Prevent event propagation to avoid triggering the document click event
+    event.stopPropagation();
+
+    // Hide all cards
+    document.querySelectorAll(".card").forEach((card) => {
+      card.style.display = "none";
+    });
+
+    feature.querySelector(".card").style.display = "block";
+  });
+});
+
+// Add a click event listener to the document
+document.addEventListener("click", () => {
+  // Hide all cards when clicking on an empty space
+  document.querySelectorAll(".card").forEach((card) => {
+    card.style.display = "none";
+  });
+});
+
